ece40a9fdf66bb60d40af27d53a078e097dc1dbc
Neto test <loktionov.dv@mail.ru>
2023-03-05 12:46:38 +0300

    Добавлен тест testRemainIf1000

--- src/test/java/ru/netology/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -37,11 +37,21 @@ public class CashbackHackServiceTest {
     }
 
     @Test
-    public void testRemainIfMore() {
+    public void testRemainIfMoreThan1000() {
+
+        CashbackHackService service = new CashbackHackService();
+        int expected = 800;
+        int actual = service.remain(1200);
+
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void testRemainIf1000() {
 
         CashbackHackService service = new CashbackHackService();
         int expected = 0;
-        int actual = service.remain(1100);
+        int actual = service.remain(1000);
 
         assertEquals(actual, expected);
     }
