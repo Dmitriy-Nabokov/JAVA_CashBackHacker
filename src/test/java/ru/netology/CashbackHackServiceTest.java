9b7622e7c74d0667b6b91c75712eadd5f1662a61
Neto test <loktionov.dv@mail.ru>
2023-03-05 13:02:31 +0300

    Тесты написаны

---
+++ src/test/java/ru/netology/CashbackHackServiceTest.java
@@ -0,0 +1,55 @@
+package ru.netology;
+
+import static org.junit.Assert.*;
+public class CashbackHackServiceTest {
+
+    @org.junit.Test
+    public void testRemainIfProbableAmount() {
+
+        CashbackHackService service = new CashbackHackService();
+        int expected = 100;
+        int actual = service.remain(900);
+
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void testRemainIfNull() {
+
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1000;
+        int actual = service.remain(0);
+
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void testRemainIfMax() {
+
+        CashbackHackService service = new CashbackHackService();
+        int expected = 1;
+        int actual = service.remain(999);
+
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void testRemainIfMoreThan1000() {
+
+        CashbackHackService service = new CashbackHackService();
+        int expected = 800;
+        int actual = service.remain(1200);
+
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.Test
+    public void testRemainIf1000() {
+
+        CashbackHackService service = new CashbackHackService();
+        int expected = 0;
+        int actual = service.remain(1000);
+
+        assertEquals(actual, expected);
+    }
+}
